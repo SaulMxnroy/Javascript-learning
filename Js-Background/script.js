03157b1da89c8a696f89604575b873ad130f543b
Saul <saul.mxnroy@gmail.com>
2022-02-23 14:05:47 -0600

    first commit

---
+++ Js-Background/script.js
@@ -0,0 +1,11 @@
+console.log("Running JS");
+const username = prompt("Enter your name");
+const email = prompt("Enter your email");
+
+let user = {
+    uName: "b",
+    email: "a"
+}
+
+console.log(user.uName)
+console.log(user.email)
